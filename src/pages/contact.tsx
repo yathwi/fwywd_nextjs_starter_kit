bffcaceccc4b946a4045be7f9b79e32a54853fef
yathwi <kaitoman.r.3@gmail.com>
2023-06-22 21:17:57 +0900

    問い合わせページまで

---
+++ src/pages/contact.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { Footer } from '@/component/fotter';
+import { PageHeader } from '@/component/pageHeader';
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [inquiry, setInquiry] = useState('');
+  const [details, setDetails] = useState('');
+
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <PageHeader />
+      <div>
+        <div className='text-center'>
+          <h1 className=' mt-10 text-xl'>お問い合わせ</h1>
+          <p className=' mt-5'>
+            お問合せ内容の確認後、担当者よりご連絡させていただきます。
+            <br />
+            各項目をご入力いただき送信ボタンをクリックしてください。
+          </p>
+        </div>
+        <div className='my-10 flex justify-center'>
+          <form onSubmit={handleFormSubmit}>
+            <div className='mt-3 flex items-center'>
+              <p className='mr-5 w-40 text-right'>お名前 (必須)</p>
+              <input
+                type='text'
+                value={name}
+                className='h-10 w-[500px] border px-2'
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className='mt-3 flex items-center'>
+              <p className='mr-5 w-40 text-right'>メールアドレス (必須)</p>
+              <input
+                type='email'
+                value={email}
+                className='h-10 w-[500px] border px-2'
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className='mt-3 flex items-center'>
+              <p className='mr-5 w-40 text-right'>お電話番号 (必須)</p>
+              <input
+                type='tel'
+                value={phone}
+                className='h-10 w-[500px] border px-2'
+                onChange={(e) => setPhone(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className='mt-3 flex items-center'>
+              <p className='mr-5 w-40 text-right'>ご住所</p>
+              <input
+                type='text'
+                value={address}
+                className='h-10 w-[500px] border px-2'
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+
+            <div className='mt-3 flex items-center'>
+              <p className='mr-5 w-40 text-right'>お問い合わせ内容</p>
+              <select
+                value={inquiry}
+                className='h-10 w-[500px] border px-2'
+                onChange={(e) => setInquiry(e.target.value)}
+              >
+                <option value=''>選択してください</option>
+                <option value='物件のお問い合せ'>物件のお問い合せ</option>
+                <option value='その他'>その他</option>
+              </select>
+            </div>
+
+            <div className='mt-3 flex '>
+              <p className='mr-5 w-40 text-right'>お問合せ詳細</p>
+              <textarea
+                rows={10}
+                value={details}
+                className='w-[500px] border px-2'
+                onChange={(e) => setDetails(e.target.value)}
+              />
+            </div>
+
+            <button type='submit'>送信</button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ src/pages/service.tsx
@@ -0,0 +1,52 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Footer } from '@/component/fotter';
+import { PageHeader } from '@/component/pageHeader';
+
+const Service = () => (
+  <div>
+    <Head>
+      <title>事業内容</title>
+    </Head>
+    <PageHeader />
+    <div className=' mx-auto mb-10 max-w-[90%]'>
+      <div className=' text-center'>
+        <h1 className=' mt-10 text-xl'>事業内容</h1>
+        <p className=' mt-5'>不動産に関するサービスを提供しています。</p>
+      </div>
+
+      <div className=' mt-10'>
+        <div className=' flex justify-center'>
+          <div className=' mr-10'>
+            <Image src='/img/hero2.jpg' width={500} height={300} alt='service1' />
+          </div>
+          <div>
+            <h2 className='mb-3 text-2xl font-bold'>01 不動産の売買・賃貸仲介</h2>
+            <p className=' text-lg'>
+              売りたい・買いたい・貸したい・借りたい
+              <br />
+              すべて弊社にご相談ください。
+            </p>
+            <p className=' text-lg'>需要と供給を繋ぎます。</p>
+          </div>
+        </div>
+
+        <div className=' mt-16 flex justify-center'>
+          <div>
+            <h2 className='mb-3 text-2xl font-bold'>02解体・地中埋設物撤去</h2>
+            <p className=' text-lg'>
+              弊社提携の解体業事業者や <br />
+              産業廃棄物処理業者により、
+            </p>
+            <p className=' text-lg'>低コストな工事を実現いたします。</p>
+          </div>{' '}
+          <div className=' ml-10'>
+            <Image src='/img/hero2.jpg' width={500} height={300} alt='service1' />
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+);
+export default Service;
