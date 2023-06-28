349514438683e15e5e600978410502641c56f855
yathwi <kaitoman.r.3@gmail.com>
2023-06-28 21:06:37 +0900

    map

--- src/component/googlemap.tsx
+++ src/component/googlemap.tsx
@@ -1,22 +1,28 @@
-import { GoogleMap, LoadScript, Marker, MarkerF } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import React, { useEffect, useState } from 'react';
 
 const MapContainer = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  // サーバーサイドではwindowがundefinedなので、初期値としてnullを使用する
+  const [windowWidth, setWindowWidth] = useState<number | null>(null);
+  useEffect(() => {
+    // コンポーネントがマウントされたときにウィンドウの幅を設定
+    setWindowWidth(window.innerWidth);
+
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const markerLabel: google.maps.MarkerLabel = {
     text: 'PROJECT JAPAN',
     fontFamily: 'sans-serif',
     fontSize: '15px',
     fontWeight: 'bold',
   };
-  useEffect(() => {
-    const handleResize = () => setWindowWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   const mapStyles = {
-    height: windowWidth < 640 ? '50vh' : '100vh',
+    // windowWidthがnullでない場合のみ条件演算子を評価
+    height: windowWidth && windowWidth < 640 ? '50vh' : '100vh',
     width: '100%',
   };
 
@@ -28,7 +34,7 @@ const MapContainer = () => {
   return (
     <LoadScript googleMapsApiKey='AIzaSyDECFb7CKhkTxCU8ZXHYZCZj1mp_Dw8_sg'>
       <GoogleMap mapContainerStyle={mapStyles} zoom={15} center={defaultCenter}>
-        <MarkerF position={defaultCenter} label={markerLabel} />
+        <Marker position={defaultCenter} label={markerLabel} />
       </GoogleMap>
     </LoadScript>
   );
