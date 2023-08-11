19b94741b123de96313ecf3204e3ae9270d59efc
yathwi <kaitoman.r.3@gmail.com>
2023-08-11 12:02:30 +0900

    info 内容を変更

--- src/component/info.tsx
+++ src/component/info.tsx
@@ -3,8 +3,7 @@ export const Info: React.FC = () => (
     <div className=' flex  items-center justify-center '>
       <p className=' mr-4 font-bold text-white'>NEWS</p>
       <p className=' w-[80%] rounded-lg  bg-white py-1 pl-3 sm:w-[40%]'>
-        {' '}
-        2023/7/15 サイトリニューアル
+       夏季休暇　2023/8/11(金)～16(火)
       </p>
     </div>
   </div>
