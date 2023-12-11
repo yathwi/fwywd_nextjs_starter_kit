90b8bccf60c313f2f24e4bfbf9d7b617d3b70745
yathwi <kaitoman.r.3@gmail.com>
2023-12-11 21:44:01 +0900

    news更新

--- src/component/info.tsx
+++ src/component/info.tsx
@@ -3,7 +3,7 @@ export const Info: React.FC = () => (
     <div className=' flex  items-center justify-center '>
       <p className=' mr-4 font-bold text-white'>NEWS</p>
       <p className=' w-[80%] rounded-lg  bg-white py-1 pl-3 sm:w-[40%]'>
-       夏季休暇　2023/8/11(金)～16(火)
+        冬季休暇　2023/12/29(金)～2024/1/8(祝)
       </p>
     </div>
   </div>
