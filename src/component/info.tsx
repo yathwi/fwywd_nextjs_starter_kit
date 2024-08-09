b9de3ae8559c71f4f0b458bee104efd80f121dc9
yathwi <kaitoman.r.3@gmail.com>
2024-08-09 18:02:24 +0900

    chore: Update VSCode settings and info component
    - Update VSCode settings to explicitly fix all source code issues on save
    - Update info component to reflect summer vacation dates
    
    Fixes #123

--- src/component/info.tsx
+++ src/component/info.tsx
@@ -3,7 +3,7 @@ export const Info: React.FC = () => (
     <div className=' flex  items-center justify-center '>
       <p className=' mr-4 font-bold text-white'>NEWS</p>
       <p className=' w-[80%] rounded-lg  bg-white py-1 pl-3 sm:w-[40%]'>
-        冬季休暇　2023/12/29(金)～2024/1/8(祝)
+        夏季休暇　2024/8/10(土)～18(日)
       </p>
     </div>
   </div>
