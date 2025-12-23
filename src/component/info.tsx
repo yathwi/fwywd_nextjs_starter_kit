4b8e2f2975abdd310a788bf627a21d019a1e94e1
yathwi <kaitoman.r.3@gmail.com>
2025-12-23 15:23:00 +0900

    冬季休暇の日付を2025年に更新

--- src/component/info.tsx
+++ src/component/info.tsx
@@ -3,7 +3,7 @@ export const Info: React.FC = () => (
     <div className=' flex  items-center justify-center '>
       <p className=' mr-4 font-bold text-white'>NEWS</p>
       <p className=' w-[80%] rounded-lg  bg-white py-1 pl-3 sm:w-[40%]'>
-        夏季休暇　2025/8/9(土)～17(日)
+        冬季休暇　2025/12/27(土)～2026/1/4(日)
       </p>
     </div>
   </div>
